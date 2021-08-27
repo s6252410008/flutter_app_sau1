85c4f4cc94e56eb6853f14243b3e39feec8edb64
s6252410008 <s6252410008@sau.ac.th>
2021-08-27 10:24:51 +0700

    Code V1

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_app_sau1/view/first_ui.dart';
+import 'package:flutter_app_sau1/view/second_ui.dart';
+
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: FirstUI(),
+    ),
+  );
+}
