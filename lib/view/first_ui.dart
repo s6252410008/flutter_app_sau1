b20b73f57e1a1249539b10a771b0a084ce7b4e83
s6252410008 <s6252410008@sau.ac.th>
2021-08-27 12:50:01 +0700

    Code V.2

--- lib/view/first_ui.dart
+++ lib/view/first_ui.dart
@@ -7,7 +7,45 @@ class FirstUI extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.amber,
+      //backgroundColor: Colors.amber,
+      appBar: AppBar(
+
+        title: Text(
+          'JOOBB'
+        ),
+        centerTitle: true,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){},
+        child: Icon(
+          Icons.add,
+          color: Colors.yellow,
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'กฤษณะ อุดมแก้ว',
+              style: TextStyle(
+                fontSize: 25.0,
+                fontWeight: FontWeight.bold,
+                color: Colors.greenAccent
+              ),
+            ),Text(
+              '6252410008',
+              style: TextStyle(
+                fontSize: 25.0,
+                fontWeight: FontWeight.bold,
+                color: Colors.yellowAccent
+              ),
+            ),
+          ],
+        ),
+      ),
+    
     );
       
     
--- lib/view/second_ui.dart
+++ lib/view/second_ui.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class SecondUI extends StatefulWidget {
-  const SecondUI({ Key? key }) : super(key: key);
+  const SecondUI({Key? key}) : super(key: key);
 
   @override
   _SecondUIState createState() => _SecondUIState();
@@ -11,10 +11,109 @@ class _SecondUIState extends State<SecondUI> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      //backgroundColor: Colors.yellow,
+      appBar: AppBar(
+        backgroundColor: Colors.green[300],
+        title: Text(
+          'JOOBB ^^',
+        ),
+        centerTitle: true,
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.search,
+            ),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.exit_to_app,
+              color: Colors.green[900],
+            ),
+          ),
+        ],
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            ListTile(
+              trailing:  Icon(
+                Icons.exit_to_app,
+                color: Colors.blue,
+              ),
+              title:  Text(
+                'ออกจากการใช้งาน',
+              ),
+              onTap: (){},
+            ),
+            Divider(
+          
+              color: Colors.red,
+            ),
+            ListTile(
+              trailing: Text(
+                '999',
+              ),
+              leading: Icon(
+                Icons.access_alarm,
+              ),
+              title: Text(
+                'ตางรางสอบ',
+              ),
+              onTap: (){},
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.table_chart,
+                color: Colors.blue,
+              ),
+              title: Text(
+                'ตารางสอน',
+              ),
+              onTap: (){},
+            ),
+            ListTile(
+              title: Text(
+                'หน้าแรก',
+              ),
+              onTap: (){},
+              
+               
+            ),
+            UserAccountsDrawerHeader(
+              otherAccountsPictures: [
+                Image.asset(
+                  'assets/images/saulogo.png',
+                ),
+              ],
+              currentAccountPicture: CircleAvatar(
+                backgroundImage: AssetImage(
+                  'assets/images/myprofile.png',
+                ) ,
+              ),
+              accountName: Text(
+                'KITSANA UDOMKEAW',
+              ),
+              accountEmail: Text(
+                'ID:6252410008',
+              ),
+              decoration: BoxDecoration(
+                image:  DecorationImage(
+                  image: AssetImage(
+                    'assets/images/bgdrawer.jpg',
+                  ),
+                )
+              ),
+            ),
+          ],
+        ),
 
-    backgroundColor: Colors.red,
-    );
-      
+      ),
     
+    
+    
+    );
   }
 }
